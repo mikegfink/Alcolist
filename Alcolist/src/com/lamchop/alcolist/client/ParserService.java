1642824668f7a57ff5fdcc040b76882cb02afb1a
AlisonClark <alison.marie.clark@gmail.com>
2014-06-09 21:56:22 -0700

    Added .gitignore file and untracked many files. Renamed parser classes to import classes since they are getting, parsing, and storing the data rather than just parsing.

--- Alcolist/src/com/lamchop/alcolist/client/ParserService.java
+++
@@ -1,5 +0,0 @@
-package com.lamchop.alcolist.client;
-
-public interface ParserService {
-
-}
---
+++ Alcolist/src/com/lamchop/alcolist/server/ImportServiceImpl.java
@@ -0,0 +1,7 @@
+package com.lamchop.alcolist.server;
+
+import com.lamchop.alcolist.client.ImportService;
+
+public class ImportServiceImpl implements ImportService {
+
+}
