d48465a109a45bfc1f78723bdf1cca83e1c24e26
Elizabeth McKeown <emckeown89@gmail.com>
2014-06-12 17:35:33 -0700

    Basic UI created - needs alot of refactoring

---
+++ Alcolist/src/com/lamchop/alcolist/client/ListPanel.java
@@ -0,0 +1,26 @@
+package com.lamchop.alcolist.client;
+
+import java.util.List;
+
+import com.google.gwt.cell.client.Cell;
+import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.user.cellview.client.DataGrid;
+import com.lamchop.alcolist.server.Manufacturer;
+
+public class ListPanel extends DataGrid<Manufacturer>{
+	
+	
+	
+//	public void onInitialize() {
+//
+//		Column<Manufacturer, Manufacturer> visibleColumn = 
+//				new Column<Manufacturer, Manufacturer>(new ManufacturerCell()) {
+//				@Override
+//	            public Manufacturer getValue(Manufacturer object) {
+//	              return object;
+//	            }
+//			};
+//		this.addColumn(visibleColumn);
+//	}
+
+}
---
+++ Alcolist/src/com/lamchop/alcolist/client/UIUpdater.java
@@ -0,0 +1,17 @@
+package com.lamchop.alcolist.client;
+
+import java.util.List;
+
+import com.lamchop.alcolist.server.Manufacturer;
+
+public class UIUpdater {
+	
+	
+	public void update(List<Manufacturer> manufacturers) {
+		
+		
+			
+	}
+	
+
+}
