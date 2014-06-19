c49b9e56f34658fc7de0ffb0872e2430f6c9690d
AlisonClark <alison.marie.clark@gmail.com>
2014-06-18 19:39:22 -0700

    Fixed JUnit test error. Maps apis can't be used on the server side.

--- Alcolist/src/com/lamchop/alcolist/server/test/LatLongAdderTest.java
+++ Alcolist/src/com/lamchop/alcolist/server/test/LatLongAdderTest.java
@@ -1,5 +1,6 @@
 package com.lamchop.alcolist.server.test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
@@ -12,7 +13,6 @@ import org.junit.Test;
 
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
-import com.google.gwt.maps.client.base.LatLng;
 import com.lamchop.alcolist.server.ImportServiceImpl;
 import com.lamchop.alcolist.server.ManufacturerServiceImpl;
 import com.lamchop.alcolist.server.PMF;
@@ -44,7 +44,8 @@ public class LatLongAdderTest {
 	public void testGeocoding() {
 		pm = PMF.getPMF().getPersistenceManager();
 		List<Manufacturer> all;
-		LatLng latLng;
+		double latitude;
+		double longitude;
 		
 		importService.importData();
 		all = manufacturerService.getManufacturers();
@@ -52,11 +53,15 @@ public class LatLongAdderTest {
 		assertTrue(all.size() > 350);
 		
 		for (Manufacturer next : all) {
-			latLng = next.getLatLng();
 			// Test that latitude and longitude have been changed from initial value 0.
 			// All locations are in BC, so none should have latitude or longitude 0.
-			assertTrue(latLng.getLatitude() != 0);
-			assertTrue(latLng.getLongitude() != 0);
+			latitude = next.getLatitude();
+			longitude = next.getLongitude();
+			System.out.println("Manufacturer " + next.getName() + "has latitude " +
+					latitude + " and longitude " + longitude);
+			assertFalse(latitude == 0);
+
+			assertFalse(longitude == 0);
 		}
 	}
 		
