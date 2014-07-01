5fb6ecd8c90fd9b9b86d9ad93ce50fb71a8d1636
Mike Fink <mike.g.fink@gmail.com>
2014-07-01 11:36:19 -0700

    Including the deleted files properly. Also the files that update from a GAE deploy.

--- Alcolist/src/com/lamchop/alcolist/client/AlcolistData.java
+++
@@ -1,5 +0,0 @@
-package com.lamchop.alcolist.client;
-
-public class AlcolistData {
-
-}
--- Alcolist/src/com/lamchop/alcolist/client/MyStyle.java
+++
@@ -1,9 +0,0 @@
-package com.lamchop.alcolist.client;
-
-import com.google.gwt.user.cellview.client.DataGrid.Style;
-import com.google.gwt.user.cellview.client.DataGrid;
-
-public interface MyStyle extends DataGrid.Style {
-	
-
-}
